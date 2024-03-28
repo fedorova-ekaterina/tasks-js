bea70b4f229b6b90d9bfcea2e2c531a716257424
ekaterina fedorova <fedorova.ekaterina@hexly.ru>
2024-03-28 20:03:39 +0300

    task2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//task1
+
 const string = "Привет! Как дела?";
 const getVowels = (str) => {
   const vowels = ['а', 'е', 'о', 'у', 'и', 'ы', 'э', 'ю', 'я'];
@@ -6,3 +8,17 @@ const getVowels = (str) => {
 }
 
 console.log(getVowels(string));
+
+//task2
+
+const workers = [
+    {"name":"John","salary":200},
+    {"name":"Mike","salary":1800},
+    {"name":"Linda","salary":1200}];
+
+const getWorthyWorkers = (arr) => {
+    const result = arr.filter((worker) => worker['salary'] > 1000).map((worker) => worker.name);
+    return result;
+  }
+  
+  console.log(getWorthyWorkers(workers));
