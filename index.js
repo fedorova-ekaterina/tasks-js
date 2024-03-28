b1341f7711c6f1b8444020e014888efabc80d31d
ekaterina fedorova <fedorova.ekaterina@hexly.ru>
2024-03-28 20:36:30 +0300

    task4

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ const getWorthyWorkers = (arr) => {
     return result;
   }
   
-  console.log(getWorthyWorkers(workers));
+console.log(getWorthyWorkers(workers));
 
-  //task3
+//task3
 
 const path = "/users/download/index.html";
 const isHtml = (path) => {
-  return path.slice(-5) === '.html';
+    return path.slice(-5) === '.html';
 }
 
-console.log(isHtml(path))
+console.log(isHtml(path));
+
+//task4
+
+const mixedArray = [3, 13, 74, 14, 66, 15, 22, 4];
+const isEven = (num) => {
+    return num % 2 === 0;
+};
+
+const filterArray = (arr, func) => {
+    return arr.filter(func);
+}
+
+console.log(filterArray(mixedArray, isEven));
